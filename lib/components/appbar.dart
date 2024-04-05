dfa344ed41575fc1ecf9e1794add9b7f95939e5c
Piotr Karaś <elexxx214@interia.pl>
2024-04-05 22:56:42 +0200

    Creating appbar

---
+++ lib/components/appbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppBar({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(50);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.grey[850],
+      centerTitle: true,
+      title: const Text("StudFooD"),
+    );
+  }
+}
